f1ec2e4cb8e62d6c541d20e4971ed109f82cf6d6
Jan Rombouts <jarombouts@bioinf.nl>
2022-05-22 14:31:11 +0200

    also uses commandline args for input

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -23,30 +23,32 @@ from get_ncbi_assignment2 import NCBIHandler
 # NB3: you need to both save the downloaded xml files and communicate the reference PUBMEDs back to the server
 
 if __name__ == "__main__":
-    IP = ''
-    PORTNUM = 5381
     AUTHKEY = b'whathasitgotinitspocketsesss?'
-    # input_args = Interface()
-    pmid = "30049270"
-    main_pmid = NCBIHandler(pmid)
+    input_args = Interface()
+    IP = input_args.args.host
+    PORTNUM = input_args.args.port
+    number_refs = input_args.args.a
+    pmid = input_args.args.pubmed_id[0]
+
+    main_pmid = NCBIHandler(pmid, number_refs)
     ref_ids = main_pmid.ncbi_query()
     main_pmid.make_dir()
-    # print(ref_ids)
-    server_start = Connect2Network(PORTNUM,AUTHKEY,IP)
-    server = mp.Process(target=server_start.runserver, args=(main_pmid.download_ncbi_refs, ref_ids))
-    server.start()
-    time.sleep(1)
-    client = mp.Process(target=server_start.runclient, args=(4,))
-    client.start()
-    server.join()
-    client.join()
+
+    # server_start = Connect2Network(PORTNUM,AUTHKEY,IP)
+    # server = mp.Process(target=server_start.runserver, args=(main_pmid.download_ncbi_refs, ref_ids))
+    # server.start()
+    # time.sleep(1)
+    # client = mp.Process(target=server_start.runclient, args=(4,))
+    # client.start()
+    # server.join()
+    # client.join()
 
 
 # 1.2.1. In this first test iteration, you need to start clients manually:
-# this means using ssh to log in to the hosts where the clients should run and starting them by hand
+# this means using ssh to log in to the hosts where the clients should run and starting them by hand CHECK
 # 1.2.2. In this first iteration, only specify the first host (the server) with the "–host": CHECK
 # this is the host where the server runs and which the clients should try to contact CHECK
-# 1.2.3. You need to specify an option "-c" or "-s" which specifies whether your script starts as a server, or as a client. CHECK
+# 1.2.3. You need to specify an option "-c" or "-s" which specifies whether your script starts as a server, or as a client.
 # You can use the argparse.addmutuallyexclusivegroup option for this! CHECK
 # 1.2.4. Perhaps superflously: if you start as a server the script needs to run on the host
 # specified in "–host" argument (otherwise you can't bind the address in QueueManager)
