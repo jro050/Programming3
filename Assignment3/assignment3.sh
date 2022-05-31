11855a68462f717d10fc9e051de847874b80ec17
Jan Rombouts <jarombouts@bioinf.nl>
2022-05-31 08:46:56 +0200

    fixed BLASTDB export

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -7,7 +7,7 @@ chmod a+x assignment3.py
 #SBATCH --partition=assemblix
 #SBATCH --mem=3000
 
-export BLASTDB=~/local-fs/datasets/refseq_protein
+export BLASTDB=~/local-fs/datasets/refseq_protein/refseq_protein
 mkdir output
 for x in {1..16} ; do /usr/bin/time -ao output/timings.txt -f "${x} %e" blastp -query MCRA.faa -db /local-fs/datasets/refseq_protein/refseq_protein -num_threads 16 -outfmt 6 >> output/blastoutput_num_threads_${x}.txt ; done
 
