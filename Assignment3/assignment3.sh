609b41f6010f92c30f734e459b2284f172a53db6
Jan Rombouts <jarombouts@bioinf.nl>
2022-05-31 11:31:59 +0200

    changed order of commands

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-chmod u+x assignment3.sh
-chmod a+x assignment3.py
+
 #SBATCH --time 7:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
+#SBATCH --job-name=BLASTPassignmentJanRombouts
 #SBATCH --partition=assemblix
-#SBATCH --mem=3000
 
+chmod u+x assignment3.sh
+chmod a+x assignment3.py
 export BLASTDB=~/local-fs/datasets/refseq_protein/refseq_protein
 mkdir output
 for x in {1..16} ; do /usr/bin/time -ao output/timings.txt -f "${x} %e" blastp -query MCRA.faa -db /local-fs/datasets/refseq_protein/refseq_protein -num_threads 16 -outfmt 6 >> output/blastoutput_num_threads_${x}.txt ; done
