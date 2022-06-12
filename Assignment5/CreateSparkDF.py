7d69cf37ab8d7a2813ec954695941025a8e263bd
Jan Rombouts <jarombouts@bioinf.nl>
2022-06-12 15:31:07 +0200

    created module to create sparkdf

---
+++ Assignment5/CreateSparkDF.py
@@ -0,0 +1,14 @@
+'''
+Module to create a SparkDF
+as explained during Lecture 5
+Assignment 5 for Programming 3
+Data Science for Life Sciences
+Author: Jan Rombouts
+'''
+
+def create_spark_df(path):
+
+
+
+
+    return spark_df
