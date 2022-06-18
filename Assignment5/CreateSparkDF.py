68fb168a9f1bd9bf3621c9ed074fb29753435377
Jan Rombouts <jarombouts@bioinf.nl>
2022-06-18 13:50:23 +0200

    more efficient renaming

--- Assignment5/CreateSparkDF.py
+++ Assignment5/CreateSparkDF.py
@@ -5,7 +5,6 @@ Assignment 5 for Programming 3
 Data Science for Life Sciences
 Author: Jan Rombouts
 '''
-
 from pyspark import SparkFiles
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
@@ -15,20 +14,20 @@ def create_spark_df(path):
     sc = SparkContext('local[16]')
     sqlContext = SQLContext(sc)
     spark_df = sqlContext.read.options(delimiter="\t", header=False).csv(SparkFiles.get(path))
-    spark_df = spark_df.withColumnRenamed("_c0","protein_accession")\
-                    .withColumnRenamed("_c1","seq_MD5_digest")\
-                    .withColumnRenamed("_c2","seq_length")\
-                    .withColumnRenamed("_c3","analysis_method")\
-                    .withColumnRenamed("_c4","sig_accession")\
-                    .withColumnRenamed("_c5","sig_description")\
-                    .withColumnRenamed("_c6","start_location")\
-                    .withColumnRenamed("_c7","stop_location")\
-                    .withColumnRenamed("_c8","score")\
-                    .withColumnRenamed("_c9","status_match")\
-                    .withColumnRenamed("_c10","date")\
-                    .withColumnRenamed("_c11","interPRO_accession")\
-                    .withColumnRenamed("_c12","interPRO_description")\
-                    .withColumnRenamed("_c13","GO_annots")\
-                    .withColumnRenamed("_c14","pathway_annots")
+    spark_df = spark_df.toDF("protein_accession",
+                            "seq_MD5_digest",
+                            "seq_length",
+                            "analysis_method",
+                            "sig_accession",
+                            "sig_description",
+                            "start_location",
+                            "stop_location",
+                            "score",
+                            "status_match",
+                            "date",
+                            "interPRO_accession",
+                            "interPRO_description",
+                            "GO_annots",
+                            "pathway_annots")
 
     return spark_df
